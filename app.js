ce378f8107decd00360dc6e634eba7bfbf1f64b9
Sébastien OUALLET <sebastien.ouallet@gmail.com>
2019-03-12 22:15:32 +0100

    Enabled CORS

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var express         = require('express');
-var path            = require('path');
-var cookieParser    = require('cookie-parser');
-var logger          = require('morgan');
-var passport        = require('passport');
+const express         = require('express');
+const path            = require('path');
+const cookieParser    = require('cookie-parser');
+const logger          = require('morgan');
+const passport        = require('passport');
+const cors            = require('cors');
 
 // Déclaration des routes 
-var authRouter      = require('./routes/authentification');
-var usersRouter     = require('./routes/users');
-var schoolsRouter   = require('./routes/ecoles');
-var classRouter     = require('./routes/classes');
-var roomsRouter     = require('./routes/salles');
-var rolesRouter     = require('./routes/roles');
-var modulesRouter   = require('./routes/modules');
-var subjectsRouter  = require('./routes/matieres');
-var lessonsRouter   = require('./routes/cours');
-var reportsRouter   = require('./routes/bulletins');
+let authRouter      = require('./routes/authentification');
+let usersRouter     = require('./routes/users');
+let schoolsRouter   = require('./routes/ecoles');
+let classRouter     = require('./routes/classes');
+let roomsRouter     = require('./routes/salles');
+let rolesRouter     = require('./routes/roles');
+let modulesRouter   = require('./routes/modules');
+let subjectsRouter  = require('./routes/matieres');
+let lessonsRouter   = require('./routes/cours');
+let reportsRouter   = require('./routes/bulletins');
 
 // Instanciation du serveur express
-var app = express();
+let app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
